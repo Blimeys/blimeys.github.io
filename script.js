ec50615b0400fcae36a0394cf0868520df604ebd
Blimeys <djeanmath@gmail.com>
2018-03-30 17:22:15 +0100

    preview 1

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $('.grid__item').hover(function(){
+        $('.project-description',this).addClass('show');
+    },
+    function(){
+        $('.project-description',this).removeClass('show');
+    });
+		$('.grid__item').click(function(){
+			$('.project-description',this).addClass('show');
+		})
+});
